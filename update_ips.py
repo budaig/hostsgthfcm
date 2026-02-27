646128715a4664bc2b358c0f1f72655a62aac8ee
budaig <budaig@users.noreply.github.com>
2026-02-27 12:45:29 +0800

    Update update_ips.py

--- update_ips.py
+++ update_ips.py
@@ -16,7 +16,7 @@ from common import write_hosts_content
 
 @retry(tries=3)
 def get_json(session: Any) -> Optional[list]:
-    url = 'https://raw.hellogithub.com/hosts.json'
+    url = 'https://raw.githubusercontent.com/budaig/hostsgthfcm/refs/heads/main/hosts.json'
     try:
         rs = session.get(url)
         data = json.loads(rs.text)
